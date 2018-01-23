639d5787565966d386d07a535e2e9ffe925c7c58
Enrro <A01221672@itesm.mx>
2018-01-23 05:57:22 -0600

    made public instead of static methods, begining to make the bfs

--- Tablero.java
+++ Tablero.java
@@ -9,16 +9,16 @@ class Tablero{
 
     }
 
-    public  boolean isTop(int pos){
+    public boolean isTop(int pos){
         return pos>2? true:false;
     }
 
-    public  boolean isBottom(int pos){
+    public boolean isBottom(int pos){
         return pos<6? true:false;
 
     }
 
-    public  boolean isRight(int pos){
+    public boolean isRight(int pos){
         if(pos==2||pos==5||pos==8){
             return false;
         }
@@ -26,18 +26,20 @@ class Tablero{
 
     }
 
-    public  boolean isLeft(int pos){
+    public boolean isLeft(int pos){
         if(pos==0||pos==3||pos==6){
             return false;
         }
         return true;
     }
 
-    public  boolean solved(){
+    public boolean solved(){
         return (Arrays.equals(this.tablero, this.objetivo));
     }
 
-    public  void printMatrix(){
+    public void move(direction)
+
+    public void printMatrix(){
         System.out.println("\n-----------");
         for(int i=0; i<9;i++){
             if(i%3==0 && i!=0){
@@ -48,7 +50,7 @@ class Tablero{
         System.out.println();
     }
 
-    public  void printObjectiveMatrix(){
+    public void printObjectiveMatrix(){
         System.out.println("\n-----------");
         for(int i=0; i<9;i++){
             if(i%3==0 && i!=0){
@@ -59,7 +61,7 @@ class Tablero{
         System.out.println();
     }
 
-    public  void swap(int[] arr,  int pos1, int pos2){
+    public void swap(int[] arr,  int pos1, int pos2){
         int valTemp = arr[pos1];
         arr[pos1] = arr[pos2];
         arr[pos2] = valTemp;
--- Tarea.java
+++ Tarea.java
@@ -10,15 +10,15 @@ import java.util.Queue;
 
 public class Tarea {
     public static void main(String[] args) {
+        Tablero juego = new Tablero();
+        Queue<Tablero> queue  = new LinkedList<Tablero>();
+        int depthLimit = 20;
+        Scanner reader = null;
         int mb = 1024*1024;
-        //Queue<int[]> queue  = new LinkedList<int[]>();
 		//Getting the runtime reference from system
 		Runtime runtime = Runtime.getRuntime();
 
         System.out.println("Hello World!");
-        Tablero juego = new Tablero();
-        int val = 1;
-        Scanner reader = null;
         try {
             reader = new Scanner(new File("data.txt"));
         } catch (FileNotFoundException e) {
@@ -27,19 +27,39 @@ public class Tarea {
         for(int j=0;j<9;j++){
             //System.out.println("j: "+ j + " k: " + k);
             juego.tablero[j]= reader.nextInt();
-            juego.objetivo[j] = val;
-            val++;
+            if(j==8){
+                juego.objetivo[j] = j+1;
+            } else{
+                juego.objetivo[j] = j+1;
+            }
         }
         juego.printMatrix();
         juego.printObjectiveMatrix();
         
         //logic goes here
-        //queue.add(juego);
+        queue.add(juego);
 
+        queue.element().printMatrix();
+        int nodesVisited = 0;
+        int depth = 0;
+        while(true){
+            Tablero node = queue.poll();
+            nodesVisited++;
+
+            // Check if current node is the goal
+            if(node.solved()){
+                System.out.println("Nodes visited: " + nodesVisited);
+            }else if(depth<depthLimit){
+                
+            }else{
+                System.out.println("Depth limit has been reached, limit depth is: " + depthLimit);
+                System.exit(0);
+            }
+        }
 
 		System.out.println("##### Heap utilization statistics [MB] #####");
         System.out.println("Used Memory: " + (runtime.totalMemory() - runtime.freeMemory()) / mb);
-        // System.out.println("son iguales: " + solved(juego));
+        System.out.println("son iguales: " + queue.element().solved());
 
 
     }
